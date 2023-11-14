5443e30172d4180ee0ece3e5b3475ce843af0596
joejoseph varghese <joejosephvarghese94@gmail.com>
2023-11-14 15:59:24 +0530

    meterial taliwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,19 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+import withMT from "@material-tailwind/react/utils/withMT";
+
+export default withMT({
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", "Roboto", "Arial", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+      },
+      colors: {
+        foundItBg: "#f7f2fa",
+        footerBg: "#1d1934",
+        limeGreen: '#0AD20A',
+      },
+    },
+    plugins: [require("tailwind-scrollbar"), require("@tailwindcss/forms")],
   },
-  plugins: [],
-}
+});
